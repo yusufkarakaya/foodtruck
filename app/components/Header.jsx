5474337757f85d20e271aebf927c5eeb3aae7a3f
Yusuf Karakaya <yusufkarakaya92@gmail.com>
2024-08-28 13:18:59 -0700

    navbar completed

---
+++ app/components/Header.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Bungee, Amatic_SC, Gloria_Hallelujah } from "next/font/google";
+
+const bungee = Bungee({ subsets: ["latin"], weight: "400" });
+const amatic = Amatic_SC({ subsets: ["latin"], weight: "700" });
+const gloria = Gloria_Hallelujah({ subsets: ["latin"], weight: "400" });
+
+const Header = () => {
+  return (
+    <header className="text-center mt-12">
+      <nav className={bungee.className}>
+        <h1 className={`text-7xl text-[#FFD700] ${amatic.className}`}>
+          Temecula Treats
+        </h1>
+        <ul className="flex justify-center gap-10 mt-10">
+          <li className="menu-item ">
+            <a className={gloria.className} href="/home">
+              HOME
+            </a>
+          </li>
+          <li className="menu-item">
+            <a className={gloria.className} href="/specials">
+              TRUCK LOCATOR
+            </a>
+          </li>
+          <li className="menu-item">
+            <a className={gloria.className} href="/menu">
+              MENU
+            </a>
+          </li>
+          <li className="menu-item">
+            <a className={gloria.className} href="/about">
+              ABOUT
+            </a>
+          </li>
+          <li className="menu-item">
+            <a className={gloria.className} href="/contact">
+              CONTACT
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
